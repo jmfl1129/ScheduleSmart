79996fac9b65df0d5e9fd3ada2cd3dba9c5c2622
Dulanga <dulanga@jayawardena.net>
2019-04-20 16:27:22 +0800

    slight changes

--- index.php
+++ index.php
@@ -8,10 +8,10 @@
 	<title>SmartSchedule</title> 
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
-	<link rel="stylesheet"href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"> 
-	<scriptsrc="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script> 
-	<scriptsrc="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script> 
-	<scriptsrc="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 
 
 </head>
@@ -24,10 +24,10 @@
 	      <a class="navbar-brand">SmartSchedule</a>
 	    </div>
 	    <ul class="nav navbar-nav">
-	      <li class="active"><a href="/smartschedulefront">Home</a></li>
-	      <li><a href="/smartschedulefront/createevent.php">Create event</a></li>
-	      <li><a href="/smartschedulefront/memberlist.php"">Members</a></li>
-	      <li><a href="/smartschedulefront/options.php">Options</a></li>
+	      <li class="active"><a href="/schedulesmart">Home</a></li>
+	      <li><a href="/schedulesmart/createevent.php">Create event</a></li>
+	      <li><a href="/schedulesmart/members.php"">Members</a></li>
+	      <li><a href="/schedulesmart/options.php">Options</a></li>
 	    </ul>
 	  </div>
 	</nav>
