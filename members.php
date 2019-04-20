b913441a30ffcd78f06a5ed1ed261cac61ce74c6
jmfl1129 <46779540+jmfl1129@users.noreply.github.com>
2019-04-20 17:06:20 +0800

    Update members.php

--- members.php
+++ members.php
@@ -6,9 +6,72 @@
 <head> <title>Members - ScheduleSmart</title> </head>
 <meta charset="utf-8"/>
 <body>
-	
-	 
+
 	<h1>Members</h1>
 	
+
+<hr>
+    <form action="members.php" method="post" >
+	    
+	<section id="modulecontent" class="span9">
+                                                            <div class="span12">
+    <h3>Carlnater McStrangelove</h3>
+        <div class="btn-group header-control">
+        <a class="btn" href="/user/44232/edit"><i class="icon-pencil"></i> Edit</a>
+    </div>
+        <div class="btn-group header-control">
+        <a class="btn btn-info" href="/user/44232/schedules/">Schedules</a>
+        <a class="btn btn-info" href="/user/44232/positions/">Positions</a>
+        <a class="btn btn-info" href="/user/44232/supervisors/">Supervisors</a>
+        <a class="btn btn-info" href="/user/44232/employees/">Employees</a>
+    </div>
+    <table class="table table-condensed">
+        <tbody>
+            <tr>
+                <th>Username</th>
+                <td>admin</td>
+            </tr>
+            <tr>
+                <th>Name</th>
+                <td>Carlnater McStrangelove</td>
+            </tr>
+            <tr>
+                <th>Role</th>
+                <td>Admin</td>
+            </tr>
+                                                <tr>
+                <th>Email</th>
+                <td>no-reply@mydomain.com</td>
+            </tr>
+                        <tr>
+                <th>Minimum Hours</th>
+                <td>10</td>
+            </tr>
+                                    <tr>
+                <th>Maximum Hours</th>
+                <td>20</td>
+            </tr>
+                                    <tr>
+                <th>Requested Hours</th>
+                <td>15</td>
+            </tr>
+                                                                        <tr>
+                <th>Color</th>
+                <td><span class="label" style="background-color: #4f51ea;">#4f51ea</span></td>
+            </tr>
+            <tr>
+                <th>Active</th>
+                <td>yes</td>
+            </tr>
+        </tbody>
+    </table>
+</div>
+    </section><!--/span9-->
+  </div><!--/row-->
+  <hr>
+</div>
+
+</form>
+<!-- referenced from OpenSkedge -->
 </body>
 </html>
