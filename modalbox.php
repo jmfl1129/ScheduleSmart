7de493a4eb587f490d12eedf31169bbb1150f573
jmfl1129 <46779540+jmfl1129@users.noreply.github.com>
2019-05-08 23:59:43 +0800

    Update modalbox.php

--- modalbox.php
+++ modalbox.php
@@ -1,25 +1,16 @@
 <div id="myModal" class="modal fade">
-    <div class="modal-dialog">
-        <div class="modal-content">
+    <div class="modal-dialog modal-lg" >
+        <div class="modal-content" style="width:1250px;">
             <div class="modal-header">
                 <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <h4 class="modal-title">Sample Model Box - Header Area</h4>
+                <h4 class="modal-title">Event details:</h4>
             </div>
-            <div class="modal-body">
-                <form>
-	  <div class="form-group">
-		<label for="recipient-name" class="control-label">Recipient:</label>
-		<input type="text" class="form-control" id="recipient-name">
-	  </div>
-	  <div class="form-group">
-		<label for="message-text" class="control-label">Message:</label>
-		<textarea class="form-control" id="message-text"></textarea>
-	  </div>
-		</form>
-            </div>
+		<textarea rows="30" cols="70">
+		
+		</textarea>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-                <button type="button" class="btn btn-primary">Save</button>
+                <button type="button" class="btn btn-default" data-dismiss="modal">Go Back</button>
+                <a class="btn btn-default" href = "/ScheduleSmart/takeaphoto_dude.php">Join</a>
             </div>
         </div>
     </div>
