04f15368518283640317bb87f852d0a7c2a07281
jmfl1129 <46779540+jmfl1129@users.noreply.github.com>
2019-05-09 00:01:40 +0800

    Update myevents.php

--- organizer/myevents.php
+++ organizer/myevents.php
@@ -1,7 +1,18 @@
 <?php
+/*
+
+
+<?phpwhile ($row = $sql->fetch(\PDO::FETCH_ASSOC))
+{
 ?>
-
-
+	 <h5 class="card-title"><?php echo $row['ename']; ?>
+	 <a class="badge badge-info"><?php echo $row['type']; ?></a>
+	 </h5>
+<?php
+}
+?>
+*/
+?>
 
 
 <!DOCTYPE html>
@@ -14,10 +25,17 @@
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-	<script src="scripts/main.js"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
 
 
 </head>
@@ -25,9 +43,9 @@
 <body>
 
 	<!-- navigation bar on top -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 		<div class="container">
-    <a class="navbar-brand" href="myevents.php">ScheduleSmart Org</a>
+    <a class="navbar-brand" href="../organizer/myevents.php">ScheduleSmart Org</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
@@ -50,6 +68,12 @@
 		<li class="nav-item">
 		  <a class="nav-link" href="../login.php">Sign in</a>
 		</li>
+		
+		<li>
+			<div class="active-pink-3 active-pink-4">
+			  <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+			</div>
+		</li>
       </ul>
     </div>
   </div>
@@ -62,109 +86,114 @@
 <div class="container">
 
   <!-- Page Heading -->
-  <h1 class="my-4">ALL your events here
+  <h1 class="my-4">Upcoming Event
   <br>
     <small>listed in pages( check it out now! )</small>
   </h1>
 
 </div>
 
-<div class="container">
-  <div class="card border-0 shadow my-5">
-    <div class="card-body p-5">
-      <h1 class="font-weight-light">Fixed Full Page Background Image</h1>
-      
-  <div class="row">
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project One</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Amet numquam aspernatur eum quasi sapiente nesciunt? Voluptatibus sit, repellat sequi itaque deserunt, dolores in, nesciunt, illum tempora ex quae? Nihil, dolorem!</p>
+	<div class="container">
+	  <div class="card border-0 shadow my-5">
+		<div class="card-body p-5">
+		  <h1 class="font-weight-light">Choose what you want</h1>
+		  
+		<div class="card-deck">
+            <div class="row justify-content-md-center">
+                <div> <h1>5PM</h1>  </div>
+                
+                <div class="col-3 d-flex align-items-stretch">
+                  <div class="card h-200">
+                    <img class="card-img-top" src="../images/Picture1.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">
+                            <h5 class="card-title">Chung Chi Chapel Service</h5>
+							<a class="btn btn-primary" data-toggle="modal" href="#myModal" id="modellink">Details</a>
+							<div class="modal-container"></div>
+						</div>
+                    </div>
+				  </div>
+                </div>
+                
+                <div class="col-3 d-flex align-items-stretch">
+                  <div class="card">
+                    <img class="card-img-top" src="../images/Picture2.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">
+                            <h5 class="card-title">Medicine Fair 2019</h5>
+                            <a class="btn btn-primary" data-toggle="modal" href="#myModal" id="modellink">Details</a>
+							<div class="modal-container"></div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="col-3 d-flex align-items-stretch">
+                  <div class="card">
+                    <img class="card-img-top" src="../images/Picture3.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">
+                            <h5 class="card-title">Meditation @ Learning Garden</h5>
+                            <a class="btn btn-primary" data-toggle="modal" href="#myModal" id="modellink">Details</a>
+							<div class="modal-container"></div>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+            </div>
+            
+            <br>    
+            
+            <!-- second row of events at a different t ime -->
+            <div class="row justify-content-md-center">
+                <div> <h1>6PM</h1>  </div>
+                
+                <div class="col-3">
+                  <div class="card h-200">
+                    <img class="card-img-top" src="../images/Picture4.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">
+                            <h5 class="card-title">Student Expo</h5>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                  </div>
+                </div>     
+                
+                <div class="col-3">
+                  <div class="card h-200">
+                    <img class="card-img-top" src="../images/Picture5.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">
+                            <h5 class="card-title">Basketball with Jo <a class="badge badge-info">Private</a></h5>
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                
+                <div class="col-3">
+                  <div class="card h-200">
+                    <img class="card-img-top" src="../images/Picture6.png" alt="Image not found ">
+                    <div class="card-body">
+                        <div class="col text-center">              
+                            
+                            <h5 class="card-title">CUSU Ball <a class="badge badge-info">CUSU</a></h5> 
+                            <a href="#" class="btn btn-primary">Details</a>
+                        </div>
+                    </div>
+                  </div>
+                </div>
+                
+                   
+                
+            </div>
+            
         </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Two</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Three</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quos quisquam, error quod sed cumque, odio distinctio velit nostrum temporibus necessitatibus et facere atque iure perspiciatis mollitia recusandae vero vel quam!</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Four</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Five</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Six</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Itaque earum nostrum suscipit ducimus nihil provident, perferendis rem illo, voluptate atque, sit eius in voluptates, nemo repellat fugiat excepturi! Nemo, esse.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Seven</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam viverra euismod odio, gravida pellentesque urna varius vitae.</p>
-        </div>
-      </div>
-    </div>
-    <div class="col-lg-3 col-md-4 col-sm-6 mb-4">
-      <div class="card h-100">
-        <a href="#"><img class="card-img-top" src="http://placehold.it/700x400" alt=""></a>
-        <div class="card-body">
-          <h4 class="card-title">
-            <a href="#">Project Eight</a>
-          </h4>
-          <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eius adipisci dicta dignissimos neque animi ea, veritatis, provident hic consequatur ut esse! Commodi ea consequatur accusantium, beatae qui deserunt tenetur ipsa.</p>
-        </div>
-      </div>
-    </div>
-  </div>
-  <!-- /.row -->
+
+		
+
+	</div>
 
   <!-- Pagination -->
   <ul class="pagination justify-content-center">
@@ -190,18 +219,44 @@
           </a>
     </li>
   </ul>
+  
+		  <div class="row justify-content-md-center">
+            <div class="form-check">
+                <input type="checkbox" class="form-check-input" id="checkOnlyFreeEvents">
+                <label class="form-check-label" for="exampleCheck1">show only events when I'm free</label>
+            </div>     
+        </div>
+		
 
-</div>
 <!-- /.container -->
   <footer id="sticky-footer" class="py-4 bg-light text-dark-50">
     <div class="container text-center">
       <small>ScheduleSmart</small>
     </div>
   </footer>
+
+
+</div>
 </div>
 
 <!-- reference startbootstrap.com/snippets/full-image-background/
-	 startbootstrap.com/snippets/portfolio-four-column/-->
+	 startbootstrap.com/snippets/portfolio-four-column/
+	 https://www.phpflow.com/jquery/create-to-create-model-window/
+	 https://mdbootstrap.com/docs/jquery/forms/search/
+-->
 
 </body>
+
+
+<script type="text/javascript">
+	$(document).ready(function(){
+		var url = "modalbox.php";
+		jQuery('#modellink').click(function(e) {
+		    $('.modal-container').load(url,function(result){
+				$('#myModal').modal({show:true});
+			});
+		});
+	});
+</script>
+
 </html>
