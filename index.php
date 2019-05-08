e5cb51c2663bf36a0a23f886ab45b81bb785d286
jmfl1129 <46779540+jmfl1129@users.noreply.github.com>
2019-05-09 00:06:28 +0800

    Update index.php

--- index.php
+++ index.php
@@ -32,7 +32,7 @@
 <body>
 
 	<!-- navigation bar on top -->
-	<nav class="navbar navbar-expand-lg navbar-light bg-light">
+	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 		<div class="container">
     <a class="navbar-brand" href="organizer/myevents.php">ScheduleSmart Org</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
