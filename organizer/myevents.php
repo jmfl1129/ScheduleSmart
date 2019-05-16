0dffb1ce43041a51a2879a9e6e9f580a1a9d794e
jmfl1129 <lee1998_90@yahoo.com>
2019-05-16 13:35:07 +0800

    Update myevents.php

--- organizer/myevents.php
+++ organizer/myevents.php
@@ -13,6 +13,7 @@ session_start();
 	$q = "SELECT * FROM events WHERE type = :name OR type = 'public';";
 	$query = $conn->prepare($q);
 	$query->bindValue(':name', $_COOKIE['organizer']);
+	echo $_COOKIE['organizer'];
 	$query->execute();
 		  
 	
@@ -27,7 +28,7 @@ session_start();
 
 	<title>ScheduleSmart</title> 
 	
-	<link rel="stylesheet" href="css/css_in_most_pages.css">
+	<link rel="stylesheet" href="../css/css_in_most_pages.css">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
@@ -39,6 +40,8 @@ session_start();
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 	
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
 
 
