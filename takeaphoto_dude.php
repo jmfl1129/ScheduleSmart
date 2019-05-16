b7dd5e4f13e6ca75bd868132817ed91644e20ff5
jmfl1129 <lee1998_90@yahoo.com>
2019-05-16 14:34:20 +0800

    Update takeaphoto_dude.php

--- takeaphoto_dude.php
+++ takeaphoto_dude.php
@@ -6,16 +6,24 @@
 <head> 
 
 	<title>ScheduleSmart</title> 
-
+	
 	<link rel="stylesheet" href="css/css_in_most_pages.css">
 	<link rel="stylesheet" href="css/css_login.css">
 
 	<meta name="viewport" content="width=device-width, initial-scale=1"> 
 	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> 
-	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap-theme.min.css">
+	
 	<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+
+
 
 	<style>
 	#container {
@@ -36,18 +44,17 @@
 <meta charset="utf-8"/>
 <body>
 
-	
 	<!-- navigation bar on top -->
 	<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
 		<div class="container">
-    <a class="navbar-brand" href="/ScheduleSmart/organizer/myevents.php">ScheduleSmart Org</a>
+    <a class="navbar-brand" href="index.php">ScheduleSmart Org</a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
     <div class="collapse navbar-collapse" id="navbarResponsive">
       <ul class="navbar-nav ml-auto">
         <li class="nav-item active">
-          <a class="nav-link" href="#">Home
+          <a class="nav-link" href="index.php">Home
                 <span class="sr-only">(current)</span>
               </a>
         </li>
@@ -57,11 +64,30 @@
         <li class="nav-item">
           <a class="nav-link" href="#">Services</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Contact</a>
-        </li>
+		<?php if(isset($_COOKIE['id'])){ 
+				if(isset($_COOKIE['organizer'])){
+		?>
 		<li class="nav-item">
-		  <a class="nav-link" href="/schedulesmart/login.php">Sign in</a>
+		  <a class="nav-link" href="organizer/myevents.php"> <?php echo $_COOKIE['name']; ?> </a>
+		</li>
+		<?php }  else { ?>
+		<li class="nav-item">
+		  <a class="nav-link" href="umyevents.php"> <?php echo $_COOKIE['name']; ?> </a>
+		</li>
+		<?php } ?>
+		<li class="nav-item">
+		  <a class="nav-link" href="logout.php">Log out</a>
+		</li>
+		<?php } else { ?>
+		<li class="nav-item">
+		  <a class="nav-link" href="login.php">Sign in</a>
+		</li>
+		<?php } ?>
+		
+		<li>
+			<div class="active-pink-3 active-pink-4">
+			  <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+			</div>
 		</li>
       </ul>
     </div>
@@ -70,8 +96,8 @@
 	<br>
 	<br>
 	<!-- END OF navigation bar on top -->
-	
-	
+
+
 <div class="container">
 
   <!-- Page Heading -->
